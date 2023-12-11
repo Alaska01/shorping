c1e18962e25976925a4045c13ec3a6a13a0bc371
Alaska01 <aadaniel108@gmail.com>
2023-12-11 16:52:43 +0100

    Modified Dummy Data

--- src/App.js
+++ src/App.js
@@ -22,7 +22,54 @@ const initialData = [{
   price: 200,
   product: "vegetable",
   qty: 0
-}, ]
+},
+
+{
+  id: 4,
+  price: 200,
+  product: "Carrots",
+  qty: 0
+},
+{
+  id: 5,
+  price: 200,
+  product: "Oranges",
+  qty: 0
+},
+{
+  id: 6,
+  price: 200,
+  product: "Mangoes",
+  qty: 0
+},
+
+{
+  id: 7,
+  price: 200,
+  product: "vegetable Oil",
+  qty: 0
+},
+
+{
+  id: 8,
+  price: 200,
+  product: "Palm Oil",
+  qty: 0
+},
+
+{
+  id: 9,
+  price: 200,
+  product: "Bread",
+  qty: 0
+},
+
+{
+  id: 10,
+  price: 200,
+  product: "Tomatoes",
+  qty: 0
+},]
 
 function handleAddToCart(){
   return
@@ -33,7 +80,7 @@ function ListItems(){
   return <>
   <div>
   <h1>List of Items to Shop From</h1>
-  {initialData.map(item=> <p key={item.id}>{item.product} {item.price} <span>Add to Cart</span></p>)}
+  {initialData.map(item=> <p key={item.id}>{item.product} {item.price} <button>Add to Cart</button></p>)}
  
   </div>
   </>
@@ -42,7 +89,12 @@ function ListItems(){
 function Cart(){
   return <>
     <div>
-      <h1>Cart</h1>
+      <h1>Cart Dummy Data</h1>
+      <p>N50,000</p>
+      <ul>
+        <li><button>-</button>rice<button>+</button> <span>Number of items in Numbers(X)</span></li>
+        <li><button>-</button>beans<button>+</button> <span>Number of items in Numbers(X)</span></li>
+      </ul>
 
     </div>
   </>
