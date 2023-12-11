bdd5e667860ecab7c614a50fa08549d543b26199
Alaska01 <aadaniel108@gmail.com>
2023-12-11 16:43:43 +0100

    Add readme and some react componets

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
 
+
+const initialData = [{
+  id: 1,
+  price: 200,
+  product: "rice",
+  qty: 0
+},
+
+{
+  id: 2,
+  price: 200,
+  product: "beans",
+  qty: 0
+}, 
+
+{
+  id: 3,
+  price: 200,
+  product: "vegetable",
+  qty: 0
+}, ]
+
+function handleAddToCart(){
+  return
+}
+
+function ListItems(){
+ 
+  return <>
+  <div>
+  <h1>List of Items to Shop From</h1>
+  {initialData.map(item=> <p key={item.id}>{item.product} {item.price} <span>Add to Cart</span></p>)}
+ 
+  </div>
+  </>
+}
+
+function Cart(){
+  return <>
+    <div>
+      <h1>Cart</h1>
+
+    </div>
+  </>
+}
+
+function DiscountCoupon(){
+  return<>
+    <label>Fair Discount Code</label>
+    <input type="text" placeholder="discount coupon" />
+  </>
+}
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {/* <h1>Hello React Cart</h1> */}
+    <DiscountCoupon />
+     <ListItems />
+     <Cart />
     </div>
   );
 }
